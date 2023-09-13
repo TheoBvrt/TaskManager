ba625217f547ac1cdf161929290e40f071280735
Théo Bouverat <theo@MacBook-Pro-de-Theo.local>
2023-09-13 18:28:21 +0200

    update

--- src/main/java/ch/theo/Options.java
+++ src/main/java/ch/theo/Options.java
@@ -1,6 +1,7 @@
 package ch.theo;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.File;
@@ -8,42 +9,85 @@ import java.io.IOException;
 
 public class Options {
     public static void AddTask() {
-        String taskName;
-        String taskDescription;
-        String taskExpirationDate;
-
-        taskName =  TaskManager.GetUserInput("Name");
-        taskDescription = TaskManager.GetUserInput("Description");
-        taskExpirationDate = TaskManager.GetUserInput("Expiration date (mm.dd.yyyy)");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        TaskManager taskManager = new TaskManager();
+        final String taskName = TaskManager.GetUserInput("Name");
+        final String taskDescription = TaskManager.GetUserInput("Description");
+        final String taskExpirationDate = TaskManager.GetUserInput("Expiration date (mm.dd.yyyy)");
         final String jsonFilePath = "src/main/java/ch/theo/data.json";
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final TaskManager taskManager = new TaskManager();
+
         try {
+            //Récupère un fichier json que je stock dans un ObjectNode
             final ObjectNode rootNode = (ObjectNode) objectMapper.readTree(new File(jsonFilePath));
-            final ObjectNode newCategory = objectMapper.createObjectNode();
+            //Je créer une node a ajouter à mon Json
+            final ObjectNode taskToAdd = objectMapper.createObjectNode();
+            //Je récupère une node Json qui s'appelle Task
             final JsonNode jsonNode = rootNode.get("Task");
+
+            //Je vérifie sur ma node Json à une sous node du même nom que la nouvelle.
             if (jsonNode.has(taskName)) {
                 System.out.println("This task already exists !");
                 BackToHome(taskManager);
                 return;
             }
-            newCategory.put("name", taskName);
-            newCategory.put("expirationData", taskExpirationDate);
-            newCategory.put("description", taskDescription);
-            rootNode.with("Task").set(taskName, newCategory);
+
+            //j'ajoute des données à ma futur node avec
+            taskToAdd.put("name", taskName);
+            taskToAdd.put("expirationData", taskExpirationDate);
+            taskToAdd.put("description", taskDescription);
+
+            //j'ajoute à la node Task la nouvelle node avec un nom et la node que j'ai créer au dessus
+            rootNode.with("Task").set(taskName, taskToAdd);
+            //j'écrit les nouvelles valeurs dans le fichier json
             objectMapper.writeValue(new File(jsonFilePath), rootNode);
-        } catch (IOException e) {
-            System.out.println("Error");
         }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println("The task has been added !");
+        BackToHome(taskManager);
     }
 
     public static void RemoveTask() {
+        final String taskToDelete = TaskManager.GetUserInput("Task name");
+        final String jsonFilePath = "src/main/java/ch/theo/data.json";
 
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final TaskManager taskManager = new TaskManager();
+
+        try {
+            final JsonNode jsonNode = objectMapper.readTree(new File(jsonFilePath));
+            final JsonNode taskNode = jsonNode.get("Task");
+            if (!taskNode.has(taskToDelete)) {
+                System.out.println("This task does not exist !");
+                BackToHome(taskManager);
+            }
+            ((ObjectNode) taskNode).remove(taskToDelete);
+            objectMapper.writeValue(new File(jsonFilePath), jsonNode);
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println("The task has been deleted !");
+        BackToHome(taskManager);
     }
 
     public static void ShowTaskList() {
+        final String jsonFilePath = "src/main/java/ch/theo/data.json";
 
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final TaskManager taskManager = new TaskManager();
+        try {
+            final JsonNode jsonFileRoot = objectMapper.readTree(new File(jsonFilePath));
+            final JsonNode taskNode = jsonFileRoot.get("Task");
+            //permet de print tous les element retourner de forEachRemaining
+            taskNode.fieldNames().forEachRemaining(System.out::println);
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+        BackToHome(taskManager);
     }
 
     public static void TaskParameter() {
